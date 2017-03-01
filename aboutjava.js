2f949c0c18e2f36c9d0968000534e7c3b38ded2b
sernesto <sernesto05@gmail.com>
2017-02-28 18:48:34 -0700

    Chapter4

--- aboutjava.js
+++ aboutjava.js
@@ -6,18 +6,21 @@ function appCost(){
   var totalCost = 0;
 //first statement will display a window if both app versions are checked
   if (document.getElementById("standard").checked && document.getElementById("premium").checked) {
-    alert("Please select only 1 option");
+    document.getElementById('priceM').value = "Please select only 1 option" ;
   }
   else if (document.getElementById("premium").checked) {
       totalCost = (premium + (premium * tax)).toFixed(2);
-      alert("Premium App is $" + premium + " with tax this comes to $" + totalCost);
+      document.getElementById('priceM').value = "$" + totalCost;
   }
   else if (document.getElementById("standard").checked) {
-    alert("Free!!!");
+    document.getElementById('priceM').value ="Free!!!";
   }
   else {
-    alert("Please select only 1 option");
-  }
+    document.getElementById('priceM').value = "Please select 1 option";
+  } 
 }
 var submitButton = document.getElementById("submit");
   submitButton.addEventListener("click", appCost, false);
+/*The first error I implemented in my code was a missing closing brace for the "else" on line 20.  I did 2 types of debuging
+the first one was I checked the console to see if any errors came up.  There was a uncaught syntax error.  Then I tried commenting out
+each section to make sure everything worked correctly on each block of the code. */
