70ee931cbb49df5f809ff61b7e617fffdc135037
sernesto <sernesto05@gmail.com>
2017-03-05 23:28:54 -0700

    chapter 5

---
+++ securityjava.js
@@ -0,0 +1,6 @@
+document.getElementById('field1').value = navigator.appName;
+document.getElementById('field2').value = navigator.appVersion;
+document.getElementById('field3').value = navigator.platform;
+document.getElementById('field4').value = navigator.userAgent;
+document.getElementById('field5').value = screen.height;
+document.getElementById('field6').value = screen.colorDepth;
